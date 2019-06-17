c1643aeda27bcaeee17f6d199dcc61164031d848
Carl Fredrik Samson <cf@samson.no>
2019-06-17 20:17:44 +0200

    first stab

--- src/main.rs
+++ src/main.rs
@@ -152,28 +152,31 @@ pub fn yield_thread() {
 // see: https://github.com/rust-lang/rfcs/blob/master/text/1201-naked-fns.md
 #[naked]
 unsafe fn switch(old: *mut ThreadContext, new: *const ThreadContext) {
-    asm!("
-        mov     %rsp, 0x00($0)
-        mov     %r15, 0x08($0)
-        mov     %r14, 0x10($0)
-        mov     %r13, 0x18($0)
-        mov     %r12, 0x20($0)
-        mov     %rbx, 0x28($0)
-        mov     %rbp, 0x30($0)
 
-        mov     0x00($1), %rsp
-        mov     0x08($1), %r15
-        mov     0x10($1), %r14
-        mov     0x18($1), %r13
-        mov     0x20($1), %r12
-        mov     0x28($1), %rbx
-        mov     0x30($1), %rbp
-        ret
+    asm!("
+        movq     $0, %rdi
+        movq     %rsp, 0x00(%rdi)
+        movq     %r15, 0x08(%rdi)
+        movq     %r14, 0x10(%rdi)
+        movq     %r13, 0x18(%rdi)
+        movq     %r12, 0x20(%rdi)
+        movq     %rbx, 0x28(%rdi)
+        movq     %rbp, 0x30(%rdi)
+
+        movq     $1, %rsi
+        movq     0x00(%rsi), %rsp
+        movq     0x08(%rsi), %r15
+        movq     0x10(%rsi), %r14
+        movq     0x18(%rsi), %r13
+        movq     0x20(%rsi), %r12
+        movq     0x28(%rsi), %rbx
+        movq     0x30(%rsi), %rbp
+        retq
         "
-    : "=*m"(old)
-    : "r"(new)
     :
-    : "alignstack" // needed to work on windows
+    :"r"(old), "r"(new)
+    : "rdi"
+    : "volatile", "alignstack"
     );
 }
 
